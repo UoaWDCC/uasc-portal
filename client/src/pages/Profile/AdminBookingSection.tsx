e02b9f79cd555fba3f84624f7385268c0e533662
ZL <ray040406@gmail.com>
2024-06-27 22:54:56 +1200

    set up admin booking table divs

--- client/src/pages/Profile/AdminBookingSection.tsx
+++ client/src/pages/Profile/AdminBookingSection.tsx
@@ -1,5 +1,10 @@
 const AdminBookingSection = () => {
-  return <div></div>
+  return (
+    <>
+      <div className="border-gray-3 h-8 w-full rounded-sm border"></div>
+      <div className="border-gray-3 h-[270px] w-full border"></div>
+    </>
+  )
 }
 
 export default AdminBookingSection
